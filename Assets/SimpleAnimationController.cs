148984882cc5cbd55011a337b5255a8b97dd5838
James Billy <james@epicguru.co.uk>
2018-02-25 15:30:43 +0100

    It's something :/

---
+++ Assets/SimpleAnimationController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAnimationController : MonoBehaviour {
+
+    public Animator Anim;
+    public PlayerMovementAdaptor Movement;
+
+    public void Update()
+    {
+        Anim.SetBool("Shoot", Input.GetMouseButton(0));
+        Anim.SetBool("ADS", Input.GetMouseButton(1));
+        Anim.SetBool("Run", Movement.HorizontalMovement.magnitude > 0.5f);
+    }
+}
